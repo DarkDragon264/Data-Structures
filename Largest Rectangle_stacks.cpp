f7522de392c181d3293b358efbd60e50dc879a90
Tyler Liston <tylerliston16@gmail.com>
2026-04-27 15:01:37 -0400

    Stacks
    starts to build data  in last out first remember that
    push → add on top
    pop  → remove from top

---
+++ Largest Rectangle_stacks.cpp
@@ -0,0 +1,129 @@
+﻿//this is the stack based solution for the largest rectangle in histogram problem
+//the problem is https://www.hackerrank.com/challenges/largest-rectangle/problem
+
+#include <bits/stdc++.h>
+#include <stack>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+string ltrim(const string&);
+string rtrim(const string&);
+vector<string> split(const string&);
+
+/*
+ * Complete the 'largestRectangle' function below.
+ *
+ * The function is expected to return a LONG_INTEGER.
+ * The function accepts INTEGER_ARRAY h as parameter.
+ */
+
+//starts to build data  in last out first remember that
+//push → add on top
+//pop  → remove from top
+//stack<int> st;
+//
+//st.push(5);   // add
+//st.pop();     // remove
+//st.top();     // see top
+//st.empty();   // check empty
+long largestRectangle(vector<int> h)
+{
+    stack<int> st;
+    long maxArea = 0;
+    int n = h.size();
+
+    for (int i = 0; i <= n; i++) {
+        int currHeight = (i == n) ? 0 : h[i];
+
+        while (!st.empty() && currHeight < h[st.top()]) {
+            int height = h[st.top()];
+            st.pop();
+
+            int width;
+            if (st.empty()) {
+                width = i;
+            }
+            else {
+                width = i - st.top() - 1;
+            }
+
+            maxArea = max(maxArea, (long)height * width);
+        }
+
+        st.push(i);
+    }
+
+    return maxArea;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string n_temp;
+    getline(cin, n_temp);
+
+    int n = stoi(ltrim(rtrim(n_temp)));
+
+    string h_temp_temp;
+    getline(cin, h_temp_temp);
+
+    vector<string> h_temp = split(rtrim(h_temp_temp));
+
+    vector<int> h(n);
+
+    for (int i = 0; i < n; i++) {
+        int h_item = stoi(h_temp[i]);
+
+        h[i] = h_item;
+    }
+
+    long result = largestRectangle(h);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string& str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string& str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string& str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
